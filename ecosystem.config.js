88720fbe29ef16a9a320e849487fb7f2b292934c
Arthals <zhuozhiyongde@126.com>
2025-11-10 02:18:59 +0800

    🐛 fix: Url

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: 'Grade.Huh.Moe',
-            script: 'PORT=3333 bun run start',
+            script: 'PORT=24702 bun run start',
             log_date_format: 'YYYY-MM-DD HH:mm:ss',
         },
     ],
