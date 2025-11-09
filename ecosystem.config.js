321d627dee42b0c62ae8b7c87531ef77dc997f89
Arthals <zhuozhiyongde@126.com>
2025-11-10 02:04:02 +0800

    ✨ feat: PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apps: [
+        {
+            name: 'Grade.Huh.Moe',
+            script: 'PORT=3333 bun run start',
+            log_date_format: 'YYYY-MM-DD HH:mm:ss',
+        },
+    ],
+};
+
